4bcefe458334f17815a39b096037c883fedbb5bf
sebastian <sebamoccagatta1@gmail.com>
2021-03-21 02:26:08 -0300

    add port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const path = require ('path');
 const app = express();
 
 //Settings
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname,'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs'); 
